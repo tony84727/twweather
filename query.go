26ef4993bfa0d335f28e8b7583f71fe4e32d6781
MinecraftXwinP <tony84727@gmail.com>
2017-10-24 13:30:58 +0800

    Added query by city name.

--- query.go
+++ query.go
@@ -1,5 +1,9 @@
 package twweather
 
+import (
+	"fmt"
+)
+
 // GetStationByTownName returns StationStatus that is matched by provided town name
 func (weather *Weather) GetStationByTownName(townName string) (*StationStatus, error) {
 	for _, location := range weather.stationList.Locations {
@@ -9,5 +13,18 @@ func (weather *Weather) GetStationByTownName(townName string) (*StationStatus, e
 			return &ret, nil
 		}
 	}
-	return nil, nil
+	return nil, fmt.Errorf("Cannot find station by town name = %s", townName)
+}
+
+func (weather *Weather) GetStationsByCityName(cityName string) (map[string]StationStatus, error) {
+	stations := make(map[string]StationStatus, 2)
+	for _, location := range weather.stationList.Locations {
+		if location.CityName == cityName {
+			stations[location.StationName] = location
+		}
+	}
+	if len(stations) == 0 {
+		return stations, fmt.Errorf("Cannot find station by city name = %s", cityName)
+	}
+	return stations, nil
 }
--- query_test.go
+++ query_test.go
@@ -1,11 +1,38 @@
 package twweather
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGetStationByTownName(t *testing.T) {
 	weather.UpdateStationStatusWithData(sampleXML)
 	station, err := weather.GetStationByTownName("橫山鄉")
 	if err != nil || station.TownSN != 78 {
 		t.Fail()
+	}
+}
+
+func testHasStation(list map[string]StationStatus, name string) error {
+	_, ok := list[name]
+	if !ok {
+		return fmt.Errorf("Missing station %s", name)
+	}
+	return nil
+}
+
+func TestGetStationsByCityName(t *testing.T) {
+	weather.UpdateStationStatusWithData(sampleXML)
+	stations, err := weather.GetStationsByCityName("新竹縣")
+	logIfError := func(err error) {
+		if err != nil {
+			t.Error(err)
+		}
+	}
+	if err != nil || len(stations) != 2 {
+		t.Fail()
+	} else {
+		logIfError(testHasStation(stations, "橫山"))
+		logIfError(testHasStation(stations, "新豐"))
 	}
 }
