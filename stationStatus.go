685a523148998209e4e0608141373a5745a073c7
MinecraftXwinP <tony84727@gmail.com>
2017-10-25 12:57:31 +0800

    Added more query to weather elements.

--- stationStatus.go
+++ stationStatus.go
@@ -1,6 +1,8 @@
 package twweather
 
-import "errors"
+import (
+	"fmt"
+)
 
 type StationStatus struct {
 	StationName string
@@ -28,15 +30,27 @@ func (s *StationStatus) testWeatherElementValid(key string) (result bool) {
 	return true
 }
 
+func (s *StationStatus) getWeatherElement(key string, humanReadable string) (element interface{}, err error) {
+	if !s.testWeatherElementValid(key) {
+		panic(fmt.Errorf("no %s data", humanReadable))
+	}
+	element, _ = s.WeatherElements[key]
+	defer func() {
+		if r := recover(); r != nil {
+			err = r.(error)
+		}
+	}()
+	return
+}
+
 func (s *StationStatus) GetTemperture(celsius bool) (tempture float64, err error) {
 	const key = "TEMP"
-
-	if !s.testWeatherElementValid(key) {
-		panic(errors.New("no temperture data"))
+	we, err := s.getWeatherElement(key, "temperture")
+	if err != nil {
+		tempture = -99
+		return
 	}
-	we, _ := s.WeatherElements[key]
 	tempture = we.(float64)
-
 	if !celsius {
 		tempture = tempture*1.8 + 32
 	}
@@ -45,12 +59,61 @@ func (s *StationStatus) GetTemperture(celsius bool) (tempture float64, err error
 
 func (s *StationStatus) GetPressure() (hPa float64, err error) {
 	const key = "PRES"
-
-	if !s.testWeatherElementValid(key) {
-		panic(errors.New("no pressure data"))
+	we, err := s.getWeatherElement(key, "pressure")
+	if err != nil {
+		hPa = -99
+		return
 	}
-	we, _ := s.WeatherElements[key]
 
 	hPa = we.(float64)
+	return
+}
+
+// GetHumidity returns percentage of relative humidity.
+func (s *StationStatus) GetHumidity() (rh int, err error) {
+	const key = "HUMD"
+	we, err := s.getWeatherElement(key, "humidity")
+	if err != nil {
+		rh = -99
+		return
+	}
+	// convert to percentage
+	rh = int(we.(float64) * 100)
+	return
+}
+
+// GetSunHours returns Sun hours
+func (s *StationStatus) GetSunHours() (hours int, err error) {
+	const key = "SUN"
+	we, err := s.getWeatherElement(key, "sun hours")
+	if err != nil {
+		hours = -99
+		return
+	}
+	hours = int(we.(float64))
+	return
+}
+
+// GetDailyRainfall returns daily rainfall of the station in millimeters.
+func (s *StationStatus) GetDailyRainfall(mm float64, err error) {
+	const key = "H_24R"
+	we, err := s.getWeatherElement(key, "daily rainfall")
+	if err != nil {
+		mm = -99
+		return
+	}
+	mm = we.(float64)
+	return
+}
+
+// GetMaximumWindSpeed returns maximum wind speed in meter per second (m/s).
+func (s *StationStatus) GetMaximumWindSpeed() (speed float64, err error) {
+	const key = "H_FX"
+	we, err := s.getWeatherElement(key, "maximum wind speed")
+	if err != nil {
+		speed = -99
+		return
+	}
+	speed = we.(float64)
 	return
 }
