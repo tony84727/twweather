4ce89965b7c963edda066b816bbf926802d51602
WinXp <tony84727@gmail.com>
2017-12-27 06:40:42 +0800

    Added city list and forecast_in_weeks.

---
+++ forecast/forecast_in_weeks.go
@@ -0,0 +1,17 @@
+package forecast
+
+import (
+	"strconv"
+)
+
+func calcWeeklyForecastDatasetID(base int, city int) string {
+	return strconv.Itoa(city*4 + base)
+}
+
+func getTwoWeekDatasetID(city int) string {
+	return "F-D0047-0" + calcWeeklyForecastDatasetID(1, city)
+}
+
+func getOneWeekDatasetID(city int) string {
+	return "F-D0047-0" + calcWeeklyForecastDatasetID(3, city)
+}
